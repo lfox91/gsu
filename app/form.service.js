1da7827d5a7ff35ba437b8bd57ce6e54047070c6
Lea Fox <lsfox91@gmail.com>
2016-07-18 15:38:23 -0700

    update express app

--- app/form.service.js
+++ app/form.service.js
@@ -88,10 +88,9 @@ var FormService = (function () {
     };
     FormService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [(typeof (_a = typeof http_1.Http !== 'undefined' && http_1.Http) === 'function' && _a) || Object])
+        __metadata('design:paramtypes', [http_1.Http])
     ], FormService);
     return FormService;
-    var _a;
 }());
 exports.FormService = FormService;
 //# sourceMappingURL=form.service.js.map
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
+
+app.use( express.static(__dirname + '/app' ) );
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
