c5f90cef6f55954d76ae05edf33f5a44f16ddad0
Lea Fox <lsfox91@gmail.com>
2016-07-18 08:57:01 -0700

    404 hack

--- app/form.service.js
+++ app/form.service.js
@@ -88,9 +88,10 @@ var FormService = (function () {
     };
     FormService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [http_1.Http])
+        __metadata('design:paramtypes', [(typeof (_a = typeof http_1.Http !== 'undefined' && http_1.Http) === 'function' && _a) || Object])
     ], FormService);
     return FormService;
+    var _a;
 }());
 exports.FormService = FormService;
 //# sourceMappingURL=form.service.js.map
