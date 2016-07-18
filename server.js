e34407dcb5234c25c2c83809dc19923bdb6bae80
Lea Fox <lsfox91@gmail.com>
2016-07-18 10:38:45 -0700

    pjson up

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const app = express();
+var path = require('path');
+
 
 app.get('/', function(req, res) {
-  res.sendFile('./index.html', function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent:', './index.html');
-    }
-  });
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function(){
