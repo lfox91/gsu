5cbdab0d2b267d0969d0ebea11055c31a5818385
Lea Fox <lsfox91@gmail.com>
2016-07-19 08:46:12 -0700

    add datatables

--- systemjs.config.js
+++ systemjs.config.js
@@ -9,12 +9,13 @@
     '@angular':                   'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     'rxjs':                       'node_modules/rxjs',
-    'angular2-datatable': 'node_modules/angular2-datatable'
+    'angular2-datatable':         'node_modules/angular2-datatable'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
     'app':                        { main: 'main.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
+    'angular2-datatable':         { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
   var ngPackageNames = [
